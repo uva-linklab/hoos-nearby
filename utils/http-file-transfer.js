de37ab2904f1f9f4b85a1a8555f28ab42438a9dc
Nabeel Nasir <nabeeln7@gmail.com>
2020-10-30 07:22:52 -0400

    fix issue with text field

--- utils/http-file-transfer.js
+++ utils/http-file-transfer.js
@@ -4,21 +4,25 @@ const fs = require('fs-extra');
 /**
  * Post the files as multi-part form data to a specified uri.
  * @param uri
- * @param files
- * @param body a js object containing any key-value pairs apart from files
+ * @param files fileName -> filePath mapping
+ * @param textFields a js object containing any key-value pairs apart from files
  */
-exports.transferFiles = function (uri, files, body) {
-	const readStreamObjects = {};
-	Object.keys(files).forEach(formField => {
-		const filePath = files[formField];
-		readStreamObjects[formField] = fs.createReadStream(filePath);
+exports.transferFiles = function (uri, files, textFields) {
+	const formData = {};
+
+	// first copy all key-vals from textFields to formData
+	Object.assign(formData, textFields);
+
+	// add file streams to fields
+	Object.keys(files).forEach(fileName => {
+		const filePath = files[fileName];
+		formData[fileName] = fs.createReadStream(filePath);
 	});
 
 	const options = {
 	    method: 'POST',
 	    uri: uri,
-	    formData: readStreamObjects,
-		body: body
+		formData: formData
 	};
 
 	return request(options);
